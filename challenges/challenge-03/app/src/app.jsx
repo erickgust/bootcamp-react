a423b46af5d978fba89bd0bacb03f3fe0b72cf0b
Erick Gustavo <erick.stazs@gmail.com>
2022-06-26 22:50:05 -0300

    Import components

--- challenges/challenge-03/app/src/app.jsx
+++ challenges/challenge-03/app/src/app.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-
-const Input = ({ children, type, className = 'input' }) => (
-  <label className='main-label'>
-    <span>{children}</span>
-    <input type={type} className={className} required />
-  </label>
-)
+import React, { useState } from 'react'
+import { Form } from './components/form'
+import { Header } from './components/header'
+import { Table } from './components/table'
 
 export function App () {
+  const [cars, setCars] = useState([])
+
   return (
-    <form>
-      <Input type='url'>Imagem</Input>
-      <Input type='text'>Marca / Modelo</Input>
-      <Input type='text'>Ano</Input>
-      <Input type='text'>Placa</Input>
-      <Input type='color' className='color'>Cor</Input>
-    </form>
+    <>
+      <Header />
+      <Form setCars={setCars} cars={cars} />
+      <Table cars={cars} />
+    </>
   )
 }
