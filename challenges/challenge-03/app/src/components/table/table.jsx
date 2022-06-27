498f96144bb06a233c52056c34f5deba6a602e3c
Erick Gustavo <erick.stazs@gmail.com>
2022-06-26 22:20:10 -0300

    Add table component

---
+++ challenges/challenge-03/app/src/components/table/table.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './table.css'
+
+export function Table ({ cars }) {
+  return (
+    <table className="cars-table">
+      <thead>
+        <tr className="table-row">
+          <th>Imagem</th>
+          <th>Modelo</th>
+          <th>Ano</th>
+          <th>Placa</th>
+          <th>Cor</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {cars.length === 0 && (
+          <tr className='no-car'>
+            <td colSpan={6}>
+              <h1>Nenhum carro registrado</h1>
+            </td>
+          </tr>
+        )}
+
+        {cars.map((car) => (
+          <tr className='table-row' key={car.plate}>
+            <td><img src={car.image} alt={car.brandModel} /></td>
+            <td>{car.brandModel}</td>
+            <td>{car.year}</td>
+            <td>{car.plate}</td>
+            <td>{car.color}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
