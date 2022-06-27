a10674db8ebb03533958a24aef899d8a794d8b41
Erick Gustavo <erick.stazs@gmail.com>
2022-06-27 09:49:51 -0300

    Add id attribute

--- challenges/challenge-03/app/src/components/input/input.jsx
+++ challenges/challenge-03/app/src/components/input/input.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import './input.css'
 
-export function Input ({ children, type, className = '' }) {
+export function Input ({ children, type, id, className = '' }) {
   const variantClassName = className && ` -${className}`
 
   return (
     <label className={'input-form' + variantClassName}>
       <span className='text'>{children}</span>
-      <input type={type} className={className || 'input'} required />
+      <input
+        type={type}
+        id={id}
+        className={className || 'input'}
+        required
+        />
     </label>
   )
 }
 
 export function InputColor ({ children, ...props }) {
   return (
-    <Input className='color' {...props}>
+    <Input className='color' type='color' {...props}>
       {children}
     </Input>
   )
