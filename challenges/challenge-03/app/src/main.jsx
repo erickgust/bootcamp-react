9385ea69dc2809002f1600974dd0b13ae5a1b91f
Erick Gustavo <erick.stazs@gmail.com>
2022-06-29 09:55:59 -0300

    Add style.css

--- challenges/challenge-03/app/src/main.jsx
+++ challenges/challenge-03/app/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
 
+import './reset.css'
+import './base.css'
+import './style.css'
+
 const root = document.querySelector('[data-js="root"]')
 
 ReactDOM.createRoot(root).render(
