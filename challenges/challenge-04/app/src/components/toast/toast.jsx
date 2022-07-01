69f3d08ca5270d3b3543d1a45de9c4e0c0cc0322
Erick Gustavo <erick.stazs@gmail.com>
2022-07-01 15:49:58 -0300

    Add StyledToast component and remove css file

--- challenges/challenge-04/app/src/components/toast/toast.jsx
+++ challenges/challenge-04/app/src/components/toast/toast.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect } from 'react'
-import './toast.css'
+import styled from 'styled-components'
+
+const StyledToast = styled.div`
+  text-align: center;
+  position: fixed;
+  inset-block-end: 30px;
+  inset-inline-start: 50%;
+  transform: translateX(-50%);
+
+  background-color: ${({ error }) => error ? '#dc143c' : '#10A81F'};
+  color: var(--white-color);
+  font-size: var(--font-size-normal);
+  font-weight: var(--font-medium);
+  padding: 10px 20px;
+  border-radius: var(--border-radius);
+`
 
 export function Toast ({ visible, toast, setVisible }) {
-  const className = 'toast' + (toast.error ? ' -error' : '')
-
   useEffect(() => {
     let id
 
@@ -21,9 +34,9 @@ export function Toast ({ visible, toast, setVisible }) {
   return (
     <>
       {visible && (
-        <div className={className}>
+        <StyledToast error={toast.error}>
           {toast.message}
-        </div>
+        </StyledToast>
       )}
     </>
   )
