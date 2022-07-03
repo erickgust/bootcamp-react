aef7590ba2cc194616f0a69fe0157cb273603ae9
Erick Gustavo <erick.stazs@gmail.com>
2022-07-02 22:15:53 -0300

    Add StyledFigure component

--- challenges/challenge-04/app/src/components/no-car/no-car.jsx
+++ challenges/challenge-04/app/src/components/no-car/no-car.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import imgUrl from './off_road.svg'
-import './no-car.css'
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure`
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  > .image {
+    width: 300px;
+  }
+
+  > .subtitle {
+    font-weight: var(--font-regular);
+    font-size: var(--font-size-normal);
+    color: var(--black-color);
+  }
+`
 
 export function NoCar () {
   return (
     <tr>
       <td colSpan={6}>
-        <figure className='no-car'>
+        <StyledFigure>
           <img src={imgUrl} alt='Carro fora da estrada' className='image' />
           <figcaption className='subtitle'>Nenhum carro registrado</figcaption>
-        </figure>
+        </StyledFigure>
       </td>
     </tr>
   )
