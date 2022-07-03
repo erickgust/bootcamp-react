bed6d33a8a9454d5b01ba5d62a965c78802b2250
Erick Gustavo <erick.stazs@gmail.com>
2022-07-02 22:22:31 -0300

    Add StyledButton component

--- challenges/challenge-04/app/src/components/button/button.jsx
+++ challenges/challenge-04/app/src/components/button/button.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import './button.css'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  border: none;
+  color: var(--white-color);
+  background-color: var(--primary-color);
+  border-radius: var(--border-radius);
+  font-size: var(--font-size-normal);
+  font-weight: var(--font-bold);
+  padding: 10px;
+  max-width: 150px;
+  width: 100%;
+  align-self: center;
+  cursor: pointer;
+  transition: background-color 100ms ease-in;
+
+  &:hover {
+    background-color: var(--btn-hover-color);
+  }
+
+`
 
 export function Button ({ children, type = 'button', handleClick }) {
   return (
-    <button type={type} className='main-button' onClick={handleClick}>
+    <StyledButton type={type} onClick={handleClick}>
       {children}
-    </button>
+    </StyledButton>
   )
 }
