8b13f44f1b728f206c6909e1f33c54429db84938
Erick Gustavo <erick.stazs@gmail.com>
2022-07-03 19:26:11 -0300

    Add StyledLabel component

--- challenges/challenge-04/app/src/components/input/input.jsx
+++ challenges/challenge-04/app/src/components/input/input.jsx
@@ -1,11 +1,67 @@
 import React from 'react'
-import './input.css'
+import styled from 'styled-components'
+
+const StyledLabel = styled.label`
+  > .text {
+    display: block;
+    font-size: var(--font-size-normal);
+    font-weight: var(--font-medium);
+    color: var(--black-color);
+    margin-bottom: 5px;
+  }
+
+  > .input {
+    display: block;
+    width: 100%;
+    padding: 10px 15px;
+    color: var(--black-color);
+    border-radius: var(--border-radius);
+    border: 1px solid var(--gray-color);
+  }
+
+  > .input:focus {
+    border-color: transparent;
+    outline: 2px solid var(--primary-color);
+  }
+
+  &.-color {
+    display: flex;
+    align-items: center;
+    width: fit-content;
+    background-color: var(--white-color);
+    border-radius: var(--border-radius);
+    border: 1px solid var(--gray-color);
+    overflow: hidden;
+  }
+
+  &.-color > .text {
+    margin: 0;
+    padding: 9px;
+  }
+
+  &.-color > .color {
+    background-color: transparent;
+    appearance: none;
+    border: none;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+  }
+
+  &.-color > .color::-webkit-color-swatch {
+    border-radius: 4px;
+    border: none;
+  }
+
+  &.-color > .color::-moz-color-swatch {
+    border-radius: 4px;
+    border: none;
+  }
+`
 
 export function Input ({ children, type, id, className = '' }) {
-  const variantClassName = className && ` -${className}`
-
   return (
-    <label className={'input-form' + variantClassName}>
+    <StyledLabel className={`-${className}`}>
       <span className='text'>{children}</span>
       <input
         type={type}
@@ -13,7 +69,7 @@ export function Input ({ children, type, id, className = '' }) {
         className={className || 'input'}
         required
         />
-    </label>
+    </StyledLabel>
   )
 }
 
